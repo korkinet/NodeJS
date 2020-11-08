432119cb3f5f64d3963b7e06a57967cc8c045535
Zaur Kogan <zaurk@codevalue.net>
2020-11-08 10:26:40 +0200

    commit

--- exercise2/client.js
+++ exercise2/client.js
@@ -1,8 +1,14 @@
-const superagent = require('superagent');
+const http = require('http');
 
 for (let i = 1; i <= 10; i++) {
   console.log(new Date(), 'Sending request', i);
-
-  superagent.get('http://localhost:4300/' + i)
-    .then(res => console.log(new Date(), 'Response received', i, res.text));
+  
+  http.get('http://localhost:4300/' + i, res => {
+    let data= '';
+    res.on('data', (chunk) =>{
+      data+= chunk;
+    });
+    
+    res.on('end', () => console.log(new Date(), 'Response received', i, data));
+  });
 }
